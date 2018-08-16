8a25a79fa0d1597fccf27aef5a0db47c0c54cfaf
Alejandro Sabogal <asabogal.git@gmail.com>
2018-08-16 23:09:20 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -7,15 +7,15 @@ class App < Sinatra::Base
   end
 
   post '/team' do
-    @name = params[:name]
-    @coach = params[:coach]
-    @pg = params[:pg]
-    @sg = params[:sg]
-    @sf = params[:sf]
-    @pf = params[:pf]
-    @c = params[:c]
-    erb :team
-  end
+   @name = params[:name]
+   @coach = params[:coach]
+   @pg = params[:pg]
+   @sg = params[:sg]
+   @pf = params[:pf]
+   @sf = params[:sf]
+   @c = params[:c]
+   erb :team
+ end
 
 
 end
